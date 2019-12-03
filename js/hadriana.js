08ed132cd71a77d788505f9c4ab56712c42fe72e
Alex Gil <colibri.alex@gmail.com>
2019-12-03 18:29:26 -0500

    created js files for fade in

---
+++ js/hadriana.js
@@ -0,0 +1,14 @@
+
+
+
+$(document).ready(function() {
+
+// This hides the grid on load, then fades it in.
+
+    $('div.grid-layout').hide();
+	$('div.grid-layout').delay(2000).fadeIn(1000);
+
+});
+
+
+
